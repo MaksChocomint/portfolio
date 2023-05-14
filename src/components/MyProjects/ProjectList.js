49a64bc612d0b3ecbcb7ac7332a2f2507abc4057
Maksim <maxprofix@gmail.com>
2023-05-14 18:10:59 +0300

    Update ProjectList.js

--- src/components/MyProjects/ProjectList.js
+++ src/components/MyProjects/ProjectList.js
@@ -1,17 +1,45 @@
 const ProjectList = [
   {
-    name: "Anime Quiz",
-    description:
-      "Anime Quiz is a game made for anime fans just like me. In it, you are invited to guess both popular anime titles and little-known ones. Since there are two modes in the game: easy and hard. While there are two types of quiz in the game: guess by preview and by description. The entire database is taken from the MyAnimeList website. Good luck and have fun playing!",
-    link: "https://github.com/MaksChocomint/anime-quiz",
-    id: 1,
-  },
-  {
     name: "Youtube Clone",
     description:
       "This application is a clone of the well-known video viewing site. My version has limited functionality in comparison with the original, but the stylish appearance and the possibilities of standard video viewing will not leave anyone indifferent!",
     link: "https://github.com/MaksChocomint/youtube-clone",
+    id: 1,
+  },
+  {
+    name: "Anime Quiz",
+    description:
+      "Anime Quiz is a game made for anime fans just like me. In it, you are invited to guess both popular anime titles and little-known ones. Since there are two modes in the game: easy and hard. While there are two types of quiz in the game: guess by preview and by description. The entire database is taken from the MyAnimeList website. Good luck and have fun playing!",
+    link: "https://github.com/MaksChocomint/anime-quiz",
     id: 2,
+  },
+  {
+    name: "AniList",
+    description:
+      "AniList is my own copy of MyAnimeList, it contains auto-update, infinite pagination, different filters and sections.",
+    link: "https://github.com/MaksChocomint/anilist",
+    id: 3,
+  },
+  {
+    name: "My Travel Journal",
+    description:
+      "Using this site you can reserve your memories of coutries you have visited. I hope simple design will be likable by you",
+    link: "https://github.com/MaksChocomint/my-travel-journal-master",
+    id: 4,
+  },
+  {
+    name: "Travel Landing",
+    description:
+      "Simple adaptive landing site. It is one of my first projects, which is coded pretty well.",
+    link: "https://github.com/MaksChocomint/travel-landing",
+    id: 5,
+  },
+  {
+    name: "Real Life is RPG Extension",
+    description:
+      "What if your life was an RPG Game, would you be more productive and motivated? This project is an incarnation of my idea of getting motivation playing the RPG games. However, playing games gives you nothing but back pains, and anyways you have motivation to play. This app will help you to visualize your life as a game!",
+    link: "https://github.com/MaksChocomint/dairy-chrome-extension",
+    id: 6,
   },
 ];
 
