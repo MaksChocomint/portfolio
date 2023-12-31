48f63c885eda0d5b08d23567c9936bfc60ba518d
Maksim <maxprofix@gmail.com>
2023-12-31 18:34:47 +0300

    Update ProjectList.js

--- src/components/MyProjects/ProjectList.js
+++ src/components/MyProjects/ProjectList.js
@@ -1,45 +1,59 @@
 const ProjectList = [
   {
+    name: "Gul'dan Reviews",
+    description:
+      "Full-stack application for writing reviews. The main feature of this application is a voice/music adding. You can create your own reviews and rate films/anime/series using this website application.",
+    link: "https://github.com/MaksChocomint/guldan-reviews",
+    id: 1,
+  },
+  {
+    name: "Front-end for Diagrams app",
+    description:
+      "This site is my help in developing a site for building functional diagrams, the script for which is built under special conditions. The full version of the site is not yet available.",
+    link: "https://github.com/MaksChocomint/diagrams-front",
+    id: 2,
+  },
+  {
     name: "Youtube Clone",
     description:
       "This application is a clone of the well-known video viewing site. My version has limited functionality in comparison with the original, but the stylish appearance and the possibilities of standard video viewing will not leave anyone indifferent!",
     link: "https://github.com/MaksChocomint/youtube-clone",
-    id: 1,
+    id: 3,
   },
   {
     name: "Anime Quiz",
     description:
       "Anime Quiz is a game made for anime fans just like me. In it, you are invited to guess both popular anime titles and little-known ones. Since there are two modes in the game: easy and hard. While there are two types of quiz in the game: guess by preview and by description. The entire database is taken from the MyAnimeList website. Good luck and have fun playing!",
     link: "https://github.com/MaksChocomint/anime-quiz",
-    id: 2,
+    id: 4,
   },
   {
     name: "AniList",
     description:
       "AniList is my own copy of MyAnimeList, it contains auto-update, infinite pagination, different filters and sections.",
     link: "https://github.com/MaksChocomint/anilist",
-    id: 3,
+    id: 5,
   },
   {
     name: "My Travel Journal",
     description:
       "Using this site you can reserve your memories of coutries you have visited. I hope simple design will be likable by you",
     link: "https://github.com/MaksChocomint/my-travel-journal-master",
-    id: 4,
+    id: 6,
   },
   {
     name: "Travel Landing",
     description:
       "Simple adaptive landing site. It is one of my first projects, which is coded pretty well.",
     link: "https://github.com/MaksChocomint/travel-landing",
-    id: 5,
+    id: 7,
   },
   {
     name: "Real Life is RPG Extension",
     description:
       "What if your life was an RPG Game, would you be more productive and motivated? This project is an incarnation of my idea of getting motivation playing the RPG games. However, playing games gives you nothing but back pains, and anyways you have motivation to play. This app will help you to visualize your life as a game!",
     link: "https://github.com/MaksChocomint/diary-chrome-extension",
-    id: 6,
+    id: 8,
   },
 ];
 
